4391540912038bb75d42ac12c2e1882e4003e9bd
Wellington <wellington87rodrigues@gmail.com/>
2025-11-05 23:19:55 -0300

    alteração na página de login

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
-import { Route, Routes } from 'react-router-dom'
-import FormCadastro from './components/FormCadastro'
-import FormLogin from './components/FormLogin'
-import './App.css'
-import FormRecSenha from './components/FormRecSenha'
+import { Route, Routes } from "react-router-dom";
+import FormCadastro from "./components/FormCadastro";
+import FormLogin from "./components/FormLogin";
+import "./App.css";
 
 function App() {
-
   return (
     <Routes>
       <Route path="/" element={<FormLogin />} />
       <Route path="/cadastro" element={<FormCadastro />} />
-      <Route path='/recSenha' element={<FormRecSenha />} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/FormCadastro.jsx
+++ src/components/FormCadastro.jsx
@@ -1,74 +1,111 @@
-import { useState } from 'react'
-import { Link } from 'react-router-dom';
-import logoTipo from '../assets/LOGOTIPO.png';
-import '../components/Form.css';
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import logoTipo from "../assets/LOGOTIPO.png";
+import "../components/Form.css";
 // import '../App.css';
 
 function FormCadastro() {
-    const [formData, setFormData] = useState({
-        name: '',
-        email: '',
-        dataNascimento: '',
-        cpf: '',
-    })
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    dataNascimento: "",
+    cpf: "",
+  });
 
-    // Função genérica para atualizar o estado quando qualquer campo do formulário mudar.
-    const handleChange = (e) => {
-        const { name, value } = e.target
-        setFormData({ ...formData, [name]: value })
-    }
+  // Função genérica para atualizar o estado quando qualquer campo do formulário mudar.
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        // Aqui você pode fazer o que quiser com os dados do formulário
-        console.log(formData)
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Aqui você pode fazer o que quiser com os dados do formulário
+    console.log(formData);
+  };
 
-    return (
-        <div className='form-container'>
-            <div className="logo">
-                <img src={logoTipo} alt="logotipo" />
-            </div>
+  return (
+    <div className="form-container">
+      <div className="logo">
+        <img src={logoTipo} alt="logotipo" />
+      </div>
 
-            <div className="login-card">
-                <h2>Formulário de Cadastro</h2>
-                <form onSubmit={handleSubmit}>
-                    <div className='input-group'>
-                        <label htmlFor="name">Nome:</label>
-                        <input type="text" id="name" name='name' value={formData.name} onChange={handleChange} required />
-                    </div>
+      <div className="login-card">
+        <h2>Formulário de Cadastro</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <label htmlFor="name">Nome:</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              required
+            />
+          </div>
 
-                    <div className='input-group'>
-                        <label htmlFor="cpf">CPF:</label>
-                        <input type="text" id="cpf" name='cpf' value={formData.cpf} onChange={handleChange} required />
-                    </div>
+          <div className="input-group">
+            <label htmlFor="cpf">CPF:</label>
+            <input
+              type="text"
+              id="cpf"
+              name="cpf"
+              value={formData.cpf}
+              onChange={handleChange}
+              required
+            />
+          </div>
 
-                    <div className='input-group'>
-                        <label htmlFor="dataNascimento">Data de Nascimento:</label>
-                        <input type="date" id="dataNascimento" name='dataNascimento' value={formData.dataNascimento} onChange={handleChange} required />
-                    </div>
+          <div className="input-group">
+            <label htmlFor="dataNascimento">Data de Nascimento:</label>
+            <input
+              type="date"
+              id="dataNascimento"
+              name="dataNascimento"
+              value={formData.dataNascimento}
+              onChange={handleChange}
+              required
+            />
+          </div>
 
-                    <div className='input-group'>
-                        <label htmlFor="email">Email:</label>
-                        <input type="email" id="email" name='email' value={formData.email} onChange={handleChange} required />
-                    </div>
+          <div className="input-group">
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+          </div>
 
-                    <div className='input-group'>
-                        <label htmlFor="password">Senha:</label>
-                        <input type="password" id="password" name='password' value={formData.password} onChange={handleChange} required />
-                    </div>
+          <div className="input-group">
+            <label htmlFor="password">Senha:</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              required
+            />
+          </div>
 
-                    <button className='btn-login' type="submit">Cadastrar</button>
-                </form>
+          <button className="btn-login" type="submit">
+            Cadastrar
+          </button>
+        </form>
 
-                <div className="signup-link">
-                    <p>
-                        <Link to="/">Faça login</Link>
-                    </p>
-                </div>
-            </div>
+        <div className="signup-link">
+          <p>
+            <Link to="/">Faça login</Link>
+          </p>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default FormCadastro;
